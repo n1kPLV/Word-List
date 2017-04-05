cf8136d7e25575ce0982bc124eaef9516415bb77
daniel <daniel@maeckelmann.de>
2017-04-05 18:40:00 +0200

    added support for latin language

--- word-list/Form1.Designer.cs
+++ word-list/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.txtText = new System.Windows.Forms.TextBox();
             this.btnStart = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pbrProgress = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.cbxLanguageSelect = new System.Windows.Forms.ComboBox();
+            this.lblNoUse1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtText
@@ -41,14 +45,14 @@
             this.txtText.Location = new System.Drawing.Point(13, 13);
             this.txtText.Multiline = true;
             this.txtText.Name = "txtText";
-            this.txtText.Size = new System.Drawing.Size(259, 207);
+            this.txtText.Size = new System.Drawing.Size(622, 360);
             this.txtText.TabIndex = 0;
             // 
             // btnStart
             // 
             this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnStart.Location = new System.Drawing.Point(197, 226);
+            this.btnStart.Location = new System.Drawing.Point(560, 421);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 1;
@@ -60,11 +64,58 @@
             // 
             this.saveFileDialog1.Filter = "Text File|*.txt";
             // 
+            // pbrProgress
+            // 
+            this.pbrProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbrProgress.Location = new System.Drawing.Point(13, 392);
+            this.pbrProgress.Name = "pbrProgress";
+            this.pbrProgress.Size = new System.Drawing.Size(622, 23);
+            this.pbrProgress.TabIndex = 2;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(12, 376);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(48, 13);
+            this.lblProgress.TabIndex = 3;
+            this.lblProgress.Text = "Progress";
+            // 
+            // cbxLanguageSelect
+            // 
+            this.cbxLanguageSelect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxLanguageSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxLanguageSelect.FormattingEnabled = true;
+            this.cbxLanguageSelect.Location = new System.Drawing.Point(76, 423);
+            this.cbxLanguageSelect.Name = "cbxLanguageSelect";
+            this.cbxLanguageSelect.Size = new System.Drawing.Size(463, 21);
+            this.cbxLanguageSelect.TabIndex = 4;
+            this.cbxLanguageSelect.SelectedIndexChanged += new System.EventHandler(this.cbxLanguageSelect_SelectedIndexChanged);
+            // 
+            // lblNoUse1
+            // 
+            this.lblNoUse1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNoUse1.AutoSize = true;
+            this.lblNoUse1.Location = new System.Drawing.Point(12, 426);
+            this.lblNoUse1.Name = "lblNoUse1";
+            this.lblNoUse1.Size = new System.Drawing.Size(58, 13);
+            this.lblNoUse1.TabIndex = 5;
+            this.lblNoUse1.Text = "Language:";
+            // 
             // Form_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(647, 456);
+            this.Controls.Add(this.lblNoUse1);
+            this.Controls.Add(this.cbxLanguageSelect);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.pbrProgress);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.txtText);
             this.Name = "Form_main";
@@ -79,6 +130,10 @@
         private System.Windows.Forms.TextBox txtText;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar pbrProgress;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.ComboBox cbxLanguageSelect;
+        private System.Windows.Forms.Label lblNoUse1;
     }
 }
 
--- word-list/Form1.cs
+++ word-list/Form1.cs
@@ -8,40 +8,201 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+
 
 namespace Word_list
 {
     public partial class Form_main : Form
     {
+        private string[] aviableLang = new string[] 
+        {
+            "none",
+            "latin",
+            "latin infinitives"
+        };
+
         public Form_main()
         {
             InitializeComponent();
+
+            cbxLanguageSelect.Items.AddRange(aviableLang);
+            cbxLanguageSelect.SelectedIndex = 0;
+            
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            string[] text = txtText.Text.Split(new char[] { ' ', '.', ',', ';', ':' },StringSplitOptions.RemoveEmptyEntries);
+            string[] text = txtText.Text.Split(new char[] { ' ', '.', ',', ';', ':' ,'\r','\n'},StringSplitOptions.RemoveEmptyEntries);
             //MessageBox.Show(text[(int)numDebWord.Value]);
             List<string> Words = new List<string>();
             foreach(string s in text)
             {
                 if (!Words.Contains(s.ToLower()))
                 {
-                    Words.Add(s.ToLower());
+					Words.Add(s.ToLower());
                 }
             }
+			//string[,] word = new string[Words.Count, 2];
+			//for (int i = 0; i<Words.LongCount();i++)
+			//{
+			//	word[i, 0] = Words[i];
+			//	word[i, 1] = "";
+			//}
+			//for(int i = 0; i < word.GetLength(0); i++)
+			//{
+			//	word[i, 1] = getTrans(word[i, 0]);
+			//}
             saveFileDialog1.ShowDialog();
+
             if (saveFileDialog1.FileName != "")
             {
-                StreamWriter write = new StreamWriter(saveFileDialog1.OpenFile());
+                StreamWriter write = new StreamWriter(saveFileDialog1.OpenFile(),Encoding.UTF8);
+                
+                //write.Encoding = Encoding.Default;
+                pbrProgress.Maximum = Words.Count;
                 foreach (string s in Words)
                 {
-                    write.WriteLine(s);
+					write.WriteLine(s+'\t'+getTrans(s,cbxLanguageSelect.SelectedIndex));
+					write.Flush();
+                    pbrProgress.Value++;
                 }
                 write.Flush();
                 write.Close();
+                MessageBox.Show("Sucess!");
+                pbrProgress.Value = 0;
             }
         }
 
+        private string getTrans(string voc, int langindex)
+        {
+            switch (aviableLang[langindex])
+            {
+                case "latin":
+                    return getLatTrans(voc, false);
+
+                case "latin infinitives":
+                    return getLatTrans(voc, true);
+
+                default:
+                    return "";
+            }
+        }
+
+		private string getLatTrans(string voc, bool inf)
+		{
+			
+			HttpWebRequest request = WebRequest.CreateHttp("https://www.latein.me/latein/" + voc);
+			request.KeepAlive = false;
+			request.ContentType = "text/html";
+			request.Date = DateTime.UtcNow;
+			WebResponse rawresponse;
+			try
+			{
+				rawresponse = request.GetResponse();
+			}
+			catch (WebException ex)
+			{
+				return ex.Message;  
+			}
+			//if(rawresponse
+			StreamReader response = new StreamReader(rawresponse.GetResponseStream());
+
+			string VocLine = "";
+			bool hasntVocLine = true;
+			while (hasntVocLine)
+			{
+				string tmp = response.ReadLine();
+				if (tmp.Contains("<div class=\"contentBox\">"))
+				{
+					hasntVocLine = false;
+					VocLine = tmp;
+				}
+			}
+			response.Close();
+            //VocLinePrep
+            //VocLine = VocLine.Replace("","");
+
+            if (inf)
+            {
+                string[] infinitives = VocLine.Split(new string[] { "<dt class=\"translationEntryBox\">" }, StringSplitOptions.RemoveEmptyEntries);
+                infinitives[0] = "";
+                for (int i = 0; i < infinitives.Length; i++)
+                {
+                    string[] tmp = infinitives[i].Split(new char[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (tmp.Length > 1)
+                    {
+                        infinitives[i] = tmp[1];
+                    }
+                }
+                //TODO!!
+                StringBuilder output = new StringBuilder();
+                foreach (string s in infinitives)
+                {
+                    if (s != "")
+                    {
+                        output.Append(s);
+                        output.Append(" oder ");
+                    }
+                }
+                if (output.Length > 6)
+                    output.Remove(output.Length - 6, 6);
+                return output.ToString();
+            }
+            else
+            {
+                //VocLineParser
+                string[] translation = VocLine.Split(new string[] { "<dd class=\"translationEntry\">" }, StringSplitOptions.RemoveEmptyEntries);
+                string[] formAnalysis = VocLine.Split(new string[] { "<dd class=\"formAnalysisEntry\">" }, StringSplitOptions.RemoveEmptyEntries);
+                Console.Write("H");
+                translation[0] = "";
+                formAnalysis[0] = "";
+                for (int i = 0; i < translation.Length; i++)
+                {
+                    string[] tmp = translation[i].Split(new char[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (tmp.Length > 1)
+                    {
+                        translation[i] = tmp[1];
+                    }
+                }
+                for (int i = 0; i < formAnalysis.Length; i++)
+                {
+                    string[] tmp = formAnalysis[i].Split(new char[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (tmp.Length > 0)
+                    {
+                        formAnalysis[i] = tmp[0];
+                    }
+                }
+                StringBuilder output = new StringBuilder();
+
+                foreach (string s in translation)
+                {
+                    if (s != "" & output.Length < 100)
+                    {
+                        output.Append(s);
+                        output.Append(" oder ");
+                    }
+                }
+                output.Remove(output.Length - 6, 6);
+                //output.Append("; Formbestimmung: ");
+                //foreach (string s in formAnalysis)
+                //{
+                //	if (s != "")
+                //	{
+                //		output.Append(s);
+                //		output.Append(" oder ");
+                //	}
+                //}
+                //output.Remove(output.Length - 6, 6);
+
+                return output.ToString(); 
+            }
+		}
+
+        private void cbxLanguageSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            object s = sender;
+        }
     }
 }
